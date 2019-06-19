4caeca1326f07a39884d3c56b91028ab357ad7a9
Felipe_Reyes <felipe.reyes@dicipa.com.mx>
2019-06-19 12:06:37 -0500

    Login con office 365

---
+++ src/helpers/config.js
@@ -0,0 +1,7 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+export const applicationId = '43f45941-6a41-4ad8-a716-561e0dade86c';
+export const redirectUri = 'http://localhost:3000/';
