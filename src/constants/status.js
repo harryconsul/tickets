f0d26e992ea645c8aebc1b32842c75bbdb86e44c
MIGUEL GUZMAN <miguel.guzman@dicipa.com.mx>
2019-07-24 17:57:47 -0500

    colors

--- src/constants/status.js
+++ src/constants/status.js
@@ -1,6 +1,6 @@
 export const NEW={value:'NU',color:"#1e88e5"};
-export const IN_PROCESS={value:'PR',color:"#9e9e9e"};
-export const SOLVED={value:'RE',color:"#00c853"};
+export const IN_PROCESS={value:'PR',color:"#f3b600"};
+export const SOLVED={value:'RE',color:"#7ebc01"};
 export const THIRD={value:'TR',color:"#002171"};
 export const REJECTED={value:'RX',color:"#d32f2f"};
 export const BY_USER={value:'RU',color:"#76ff03"};
