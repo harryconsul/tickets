51fd2081d2fd56fe73f40e656629a8d063d8d068
MIGUEL GUZMAN <miguel.guzman@dicipa.com.mx>
2019-08-07 16:54:15 -0500

    imagen default

--- src/components/EngineerAvatar.js
+++ src/components/EngineerAvatar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Typography } from '@material-ui/core';
+import fallBack from '../assets/persona.png';
 
 const EngineerAvatar = props =>{
     const style = {width:'100px',height:'100px',borderRadius:'50px',...props.style};
     return(
        <div style={{position:'relative',float:'right'
        ,right:'10px',bottom:'40px',width:'100px',display:'flex',flexDirection:"column"}}>
-            <img src={props.photo} alt="Avatar del responsable del servicio" style={style} />
+             
+             <img src={props.photo===""?fallBack:props.photo} alt="Avatar del responsable del servicio" style={style} />
+           
+            
            
                 <Typography variant={"caption"} style={{textAlign:"center"}}>
                     <i>
