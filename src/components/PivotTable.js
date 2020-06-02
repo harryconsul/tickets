1e31bcf21e2bed47d50a6b5627cb64f37b069864
Felipe_Reyes <felipe.reyes@dicipa.com.mx>
2020-06-02 11:42:47 -0500

    Nuevo reporteador

--- src/components/PivotTable.js
+++ src/components/PivotTable.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import * as PivotTableDR from './PivotTableDR';
-//import * as FlexmonsterReact from 'react-flexmonster';
+import * as FlexmonsterReact from 'react-flexmonster';
 //import 'flexmonster/flexmonster.min.css';
 
 import axios from 'axios';
-import {Paper} from '@material-ui/core'
+import { Paper } from '@material-ui/core'
 class PivotTable extends React.Component {
-    state={
-        ref:React.createRef(),
+    state = {
+        ref: React.createRef(),
     }
-    postData=()=>{
-        console.warn('rango',this.props.data);  
-        axios.post(this.props.webService,this.props.data).then((response)=>{
-            const report = {...this.props.report};
+    postData = () => {
+        axios.post(this.props.webService, this.props.data).then((response) => {
+            const report = { ...this.props.report };
             report.dataSource.data = response.data.SDTData;
-            console.warn(this.state.ref.current);
-            //const flex = this.state.ref.current.flexmonster;            
-            //flex.setReport(report);
+            const datarocks = this.state.ref.current.webdatarocks;
+            datarocks.setReport(report);
 
         });
     }
-    componentDidMount(){
+    componentDidMount() {
 
         this.postData();
 
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
         this.postData();
     }
-   
-    render() { 
+
+    render() {
         return (
-            <Paper style={{padding:"10px"}} >
-                {/*
-                <FlexmonsterReact.Pivot  ref={this.state.ref}                          
-                 toolbar={true}
-                 report={this.state.report}
-                 licenseKey="Z7QE-XCC244-3E3C4S-193J3N"
-                     />
-                */
-                    <PivotTableDR.Pivot toolbar={true} report="https://cdn.webdatarocks.com/reports/report.json"/>
+            <Paper style={{ padding: "10px" }} >
+                {
+                    <PivotTableDR.Pivot toolbar={true} ref={this.state.ref}
+                        toolbar={true}
+                        report={this.state.report} />
                 }
             </Paper>
         )
